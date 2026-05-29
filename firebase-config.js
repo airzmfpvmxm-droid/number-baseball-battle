aefdf76232d093cef6df0d3b89ac4fa2a2bbf8a0
airzmfpvmxm-droid <airzmfpvmxm@gmail.com>
2026-05-29 11:41:22 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Firebase Console에서 복사한 firebaseConfig 값을 아래에 붙여넣으세요.
 // 예시의 "여기에_..." 부분을 실제 값으로 바꾸면 됩니다.
 export const firebaseConfig = {
-  apiKey: "여기에_apiKey",
-  authDomain: "여기에_authDomain",
-  projectId: "여기에_projectId",
-  storageBucket: "여기에_storageBucket",
-  messagingSenderId: "여기에_messagingSenderId",
-  appId: "여기에_appId"
+  apiKey: "AIzaSyDqAnL9UtyIt__4xf2_QVfH76Z84KATu7U",
+  authDomain: "number-base-ball.firebaseapp.com",
+  projectId: "number-base-ball",
+  storageBucket: "number-base-ball.firebasestorage.app",
+  messagingSenderId: "899029664073",
+  appId: "1:899029664073:web:fb5e1177c9691667fe1a98"
 };
