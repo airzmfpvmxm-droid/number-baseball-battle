d467d915c591831ce7842770221ffe4e4005a6e8
airzmfpvmxm-droid <airzmfpvmxm@gmail.com>
2026-05-29 11:38:03 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Console에서 복사한 firebaseConfig 값을 아래에 붙여넣으세요.
+// 예시의 "여기에_..." 부분을 실제 값으로 바꾸면 됩니다.
+export const firebaseConfig = {
+  apiKey: "여기에_apiKey",
+  authDomain: "여기에_authDomain",
+  projectId: "여기에_projectId",
+  storageBucket: "여기에_storageBucket",
+  messagingSenderId: "여기에_messagingSenderId",
+  appId: "여기에_appId"
+};
